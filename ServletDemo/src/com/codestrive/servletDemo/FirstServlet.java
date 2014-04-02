05c4b4d1db0bc7cf57b1146e75e30e5c69d7ef79
Koushik Ghosh <koushik08oct@gmail.com>
2014-04-02 08:05:36 +0530

    ServletDemo
    Servlet approach with XML config and annotation configuration

---
+++ ServletDemo/src/com/codestrive/servletDemo/FirstServlet.java
@@ -0,0 +1,35 @@
+package com.codestrive.servletDemo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+@WebServlet(description = "my first servlet", urlPatterns = { "/FirstServlet" })
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("my servlet");
+		PrintWriter writer = response.getWriter();
+		writer.println("my servlet");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
